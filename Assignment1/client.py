58cfc0d5178c8435ccbcd59c7142a6961122a276
Sam Kushell <samkushell@macbook-pro-91.lan>
2018-09-11 12:12:58 -0700

    progress on part 2

--- Assignment1/client.py
+++ Assignment1/client.py
@@ -1,5 +1,33 @@
 import socket
 import argparse
+import threading
+import thread
+import sys
+
+## TODO: can't quit, hung up on recvfrom?
+
+class MessageListenerThread (threading.Thread):
+	def stopListening(self):
+		self.shouldRun = False
+	def __init__(self, name, socket, bufferLength, log):
+		threading.Thread.__init__(self)
+		self.name = name
+		self.socket = socket
+		self.bufferLength = bufferLength
+		self.log = log
+		self.shouldRun = True
+		self.daemon = True
+	def run(self):
+		print(self.name + "# waiting for messages...")
+		while(self.shouldRun):
+			try:
+				data, server_detail = self.socket.recvfrom(self.bufferLength)
+				if (data):
+				 	print (self.name + "# " + data)
+				 	self.log.write(data + "\n")
+			except:
+				self.shouldRun = False
+
 
 parser = argparse.ArgumentParser(conflict_handler= "resolve")
 parser.add_argument("-s", metavar = "<serverIP>",type = str, help = "indicates the server IP address", dest = "ip")
@@ -13,34 +41,37 @@ args = parser.parse_args()
 server_address = (args.ip, args.port)
 registerMsg = "register " + args.name
 welcomeMsgCheck = "welcome " + args.name
-logfile = open(args.logfile, "w")
+logfile = open(args.logfile, "w", 0)
 shouldConnect = False
 
 # create socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
+clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# create thread to listen to server
+listener = MessageListenerThread(args.name, clientSocket, 1024, logfile)
 try:
 	# attempt connection
 	logfile.write("connecting to the server " + args.ip + " at port " + str(args.port) + "\n")
-	sock.sendto(registerMsg, server_address)
+	clientSocket.sendto(registerMsg, server_address)
 	logfile.write("sending register message " + args.name + "\n")
   	# verify connection from server
-	data, server_detail = sock.recvfrom(1024)
-	print data
+	data, server_detail = clientSocket.recvfrom(1024)
 	if (data == welcomeMsgCheck):
 		logfile.write("recieved welcome\n")
-		print(args.name + " connected to server and registered")
-		print(args.name + " waiting for messages...")
+		#clientSocket.bind(server_address)
+		print(args.name + "# connected to server and registered")
 		shouldConnect = True;
-	else: 
-		print "oh no!"
-	while (shouldConnect):
-		user_input=str(raw_input(">"))
-		if (user_input == "exit"):
-			shouldConnect = False;
+		listener.start()
+		while (shouldConnect):
+			user_input = str(raw_input(args.name + "# "))
+			if (user_input == "exit"):
+				shouldConnect = False;
+			elif(user_input):
+				clientSocket.sendTo(user_input, server_address)
+				log.write(user_input)
 except:
 	print "Something went wrong while connecting to server"
 finally:
-	logfile.write("terminating client...")
+	logfile.write("terminating client...\n")
 	logfile.close()
-	sock.close()
+	listener.stopListening()
+	sys.exit()
--- Assignment1/server.py
+++ Assignment1/server.py
@@ -1,6 +1,54 @@
 import socket
 import argparse
+import thread
+import threading
+import sys
 
+
+class ServerThreadDictionary :
+	dictionary = {}
+	handler = -1
+	@staticmethod
+	def append(self, name, serverThread):
+		dictionary[name] = serverThread
+	@staticmethod
+	def sendMessageFromTo(socket, message, sourceName, targetName):
+		if(targetName in dictionary):
+			toSend = "recvfrom " + sourceName + message
+			socket.sendto(toSend, dictionary[targetName].address)
+			log.write( "recvfrom " + sourceName + " to  " + targetName + " \"" + message + "\"\n")
+		else:
+			if (handler == 0):
+				return
+			if (handler == 1):
+				return
+
+
+class ServerThread (threading.Thread):
+	def __init__(self, name, address, buffSize, log, serverSocket):
+		threading.Thread.__init__(self)
+		self.name = name
+		self.address = address
+		self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		self.socket.bind(address)
+		self.buffSize = buffSize
+		self.log = log
+		self.serverSocket = serverSocket
+		self.daemon = True
+	def run(self):
+		while(True):
+			data, client_address = self.socket.recvfrom(self.buffSize)
+			if (data):
+				splitData = data.split(" ", 2)
+				if (splitData[0] == "sendto"):
+					targetClientName = splitData[1]
+					rawMessage = splitData[2]
+					log.write("sendto " + targetClientName + " from " + self.name + " \"" + rawMessage + "\"")
+					ServerThreadDictionary.sendMessageFromTo(serverSocket, rawMessage, self.name, targetClientName)
+
+def sendDirectMessage(message, targetClientName):
+	if (message):
+		return
 
 # parse initial commands
 parser = argparse.ArgumentParser(conflict_handler= "resolve")
@@ -8,34 +56,40 @@ parser.add_argument("-p", metavar = "portno", type = int, help = "the port numbe
 parser.add_argument("-l", metavar = "logfile", help = "name of the logfile", dest = "logfile")
 parser.add_argument("-h", metavar = "handler", type = int, help = "indicates how unknown clients handled", dest = "handler")
 args = parser.parse_args()
-
 # open file
-logfile = open(args.logfile, "w")
-
+logfile = open(args.logfile, "w", 0)
+# make client list
 # initialize socket
 ip = "localhost";
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)        # Create a socket object
-s.bind((ip, args.port))                                     # Bind to the port
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)        # Create a socket object
+serverSocket.bind((ip, args.port))                                     # Bind to the port
 logfile.write("server started on " + ip + " at port " + str(args.port) + "...\n");
-
 # do server work
 buffSize = 1024
-while (True):
-	# listen for new connections
-	clientName = ""
-	data, client_address = s.recvfrom(buffSize)
-	logfile.write("client connection from host " + client_address[0] + " port " + str(client_address[1]) + "\n")
-	if (data):
-		splitData = data.split(" ")
-		if (splitData[0] == 'register'):
-			logfile.write("recieved " + data + " from host " + client_address[0] + " port " + str(client_address[1]) + "\n")
-			welcomeMsg = "welcome " + splitData[1]
-			s.sendto(welcomeMsg, client_address)
-			print(splitData[1] + " registered from host " + client_address[0] + " port " + str(client_address[1]))
-# teardown
-logfile.write("terminating server...")
-logfile.close()
-s.close()
+try:
+	while (True):
+		# listen for new connections
+		data, client_address = serverSocket.recvfrom(buffSize)
+		logfile.write("client connection from host " + client_address[0] + " port " + str(client_address[1]) + "\n")
+		if (data):
+			splitData = data.split(" ")
+			if (splitData[0] == 'register'):
+				clientName = splitData[1]
+				logfile.write("recieved " + data + " from host " + client_address[0] + " port " + str(client_address[1]) + "\n")
+				welcomeMsg = "welcome " + clientName
+				# send validation
+				print "here"
+				serverSocket.sendto(welcomeMsg, client_address)
+				# create ServerThread for client
+				newClient = ServerThread(clientName, client_address, buffSize, logfile, serverSocket)
+				print(splitData[1] + " registered from host " + client_address[0] + " port " + str(client_address[1]))
+
+# handle shutdown
+except KeyboardInterrupt:
+	logfile.write("terminating server...\n")
+	logfile.close()
+	serverSocket.close()
+	sys.exit()
 
 
 
